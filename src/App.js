122ad19bed0328b1d13bf54694baf038a2af7fa9
Christopher Inman <christophereinman@gmail.com>
2021-04-13 21:02:51 -0500

    changed order of links

--- src/App.js
+++ src/App.js
@@ -8,14 +8,14 @@ function App() {
   return (
     <div>
       <Switch>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
         <Route path="/favorites">
           <FavoritesPage />
         </Route>
         <Route path="/new-meetup">
           <NewMeetupPage />
-        </Route>
-        <Route path="/">
-          <AllMeetupsPage />
         </Route>
       </Switch>
     </div>
