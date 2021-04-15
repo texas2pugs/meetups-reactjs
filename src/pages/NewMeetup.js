741f83d63c335a88926bbeebf313b2ce4e5c172e
Christopher Inman <christophereinman@gmail.com>
2021-04-14 21:10:01 -0500

    Sending HTTP req to firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,20 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm.js';
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch('https://meetups-reactjs-default-rtdb.firebaseio.com/meetups.json', {
+      method: 'POST',
+      body: JSON.stringify(meetupData),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
   return (
     <section>
       <h1>New Meetup Page</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
