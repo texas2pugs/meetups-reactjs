74c9634ec3795ffb4fe0ef75eaf28e7808a0586c
Christopher Inman <christophereinman@gmail.com>
2021-04-14 21:45:56 -0500

    Navigating programmatically

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,10 @@
+import { useHistory } from 'react-router-dom';
+
 import NewMeetupForm from '../components/meetups/NewMeetupForm.js';
 
 function NewMeetupPage() {
+  const history = useHistory();
+
   function addMeetupHandler(meetupData) {
     fetch('https://meetups-reactjs-default-rtdb.firebaseio.com/meetups.json', {
       method: 'POST',
@@ -8,6 +12,8 @@ function NewMeetupPage() {
       headers: {
         'Content-Type': 'application/json',
       },
+    }).then(() => {
+      history.replace('/');
     });
   }
 
